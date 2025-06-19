ee84380c0017c6caff9ce56e76651a3ae30ade68
Kukushenok <gusarov.ml@mail.ru>
2025-06-19 15:26:39 +0300

    новые ссылки

--- lend-react/src/components/Footer/Footer.tsx
+++ lend-react/src/components/Footer/Footer.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer: React.FC = () => {
+  const handleOpenPdf = (name: string) => {
+    const pdfUrls: { [key: string]: string } = {
+      'zelikov': '/Зеликов Игорь.pdf',
+      'zelikova': '/Зеликова Анна.pdf'
+    };
+    
+    window.open(pdfUrls[name], '_blank');
+  };
+
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer__content">
           <div className="footer__row">
-            <p className="footer__ip">ИП Зеликов Игорь Сергеевич</p>
-            <p className="footer__inn">ИНН Зеликов: 673209938317</p>
-          </div>
-          <div className="footer__row">
-            <p className="footer__ip">ИП Зеликова Анна Николаевна</p>
-            <p className="footer__inn">ИНН Зеликова: 673211217915</p>
+            <div className="footer__doc-block" onClick={() => handleOpenPdf('zelikov')}>
+              <p className="footer__ip">ИП Зеликов Игорь Сергеевич</p>
+              <p className="footer__inn">ИНН: 673209938317</p>
+            </div>
+            <div className="footer__doc-block" onClick={() => handleOpenPdf('zelikova')}>
+              <p className="footer__ip">ИП Зеликова Анна Николаевна</p>
+              <p className="footer__inn">ИНН: 673211217915</p>
+            </div>
           </div>
           <div className="footer__phone">
             <p>
